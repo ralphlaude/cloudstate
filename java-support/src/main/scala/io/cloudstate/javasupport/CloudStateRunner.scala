cdcefe35a0949faba47db509f1bce1ea9140e497
Guy Youansi <ralphlaude@yahoo.fr>
2020-05-25 18:40:22 +0200

    remove unused import

--- java-support/src/main/scala/io/cloudstate/javasupport/CloudStateRunner.scala
+++ java-support/src/main/scala/io/cloudstate/javasupport/CloudStateRunner.scala
@@ -26,7 +26,13 @@ import akka.http.scaladsl.model._
 import akka.stream.{ActorMaterializer, Materializer}
 import com.google.protobuf.Descriptors
 import io.cloudstate.javasupport.impl.eventsourced.{EventSourcedImpl, EventSourcedStatefulService}
-import io.cloudstate.javasupport.impl.{EntityDiscoveryImpl, ResolvedServiceCallFactory, ResolvedServiceMethod, StatelessFunctionImpl, StatelessFunctionService}
+import io.cloudstate.javasupport.impl.{
+  EntityDiscoveryImpl,
+  ResolvedServiceCallFactory,
+  ResolvedServiceMethod,
+  StatelessFunctionImpl,
+  StatelessFunctionService
+}
 import io.cloudstate.javasupport.impl.crdt.{CrdtImpl, CrdtStatefulService}
 import io.cloudstate.protocol.crdt.CrdtHandler
 import io.cloudstate.protocol.entity.EntityDiscoveryHandler
@@ -102,7 +108,7 @@ final class CloudStateRunner private[this] (_system: ActorSystem, services: Map[
           route orElse CrdtHandler.partial(crdtImpl)
 
         case (route, (serviceClass, statelessFunctionServices: Map[String, StatelessFunctionService] @unchecked))
-          if serviceClass == classOf[StatelessFunctionService] =>
+            if serviceClass == classOf[StatelessFunctionService] =>
           val statelessFunctionImpl = new StatelessFunctionImpl(system, statelessFunctionServices, rootContext)
           route orElse StatelessFunctionHandler.partial(statelessFunctionImpl)
 
