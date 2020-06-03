759657cd4af07ad091631182e4870af373b85f33
Guy Youansi <ralphlaude@yahoo.fr>
2020-06-03 22:04:18 +0200

    Add metrics service for stateless with tests

--- samples/java-shopping-cart/src/main/java/io/cloudstate/samples/shoppingcart/MetricsService.java
+++ samples/java-shopping-cart/src/main/java/io/cloudstate/samples/shoppingcart/MetricsService.java
@@ -1,12 +1,14 @@
 package io.cloudstate.samples.shoppingcart;
 
-import com.example.stateless.metricservice.Metricservice.AverageResult;
+import com.example.stateless.metricservice.Metricservice.Average;
 import com.example.stateless.metricservice.Metricservice.Metric;
 import io.cloudstate.javasupport.function.CommandContext;
 import io.cloudstate.javasupport.function.CommandHandler;
 import io.cloudstate.javasupport.function.Stateless;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /** An stateless function. */
 @Stateless
@@ -18,15 +20,31 @@ public class MetricsService {
   public MetricsService() {}
 
   @CommandHandler
-  public AverageResult average(List<Metric> metrics, CommandContext ctx) {
-    long sum = metrics.stream().map(Metric::getMetric).reduce(0L, (acc, value) -> acc + value);
-    return AverageResult.newBuilder().setValue(sum / metrics.size()).build();
+  public Average collect(Metric metric, CommandContext ctx) {
+    return Average.newBuilder().setValue(metric.getValue()).build();
   }
 
   @CommandHandler
-  public AverageResult streamAverage(Metric metric, CommandContext ctx) {
+  public Average collectStreamIn(List<Metric> metrics, CommandContext ctx) {
+    long sum = metrics.stream().map(Metric::getValue).reduce(0L, (acc, value) -> acc + value);
+    return Average.newBuilder().setValue(sum / metrics.size()).build();
+  }
+
+  @CommandHandler
+  public List<Average> collectStreamOut(Metric metric, CommandContext ctx) {
+    return Stream.iterate(1L, i -> i <= 100, i -> i + 1)
+        .map(
+            i -> {
+              long avg = (metric.getValue() + i) / i;
+              return Average.newBuilder().setValue(avg).build();
+            })
+        .collect(Collectors.toList());
+  }
+
+  @CommandHandler
+  public Average collectStreamed(Metric metric, CommandContext ctx) {
     count++;
-    sum += metric.getMetric();
-    return AverageResult.newBuilder().setValue(sum / count).build();
+    sum += metric.getValue();
+    return Average.newBuilder().setValue(sum / count).build();
   }
 }
